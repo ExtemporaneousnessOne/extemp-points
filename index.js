252e9c3c6e68277bff4d58cbf20750df810c140c
ExtemporaneousnessOne <133890411+ExtemporaneousnessOne@users.noreply.github.com>
2025-09-04 20:10:53 -0400

    add percents

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log('index.js loaded')
+function percentTillGoal(p){
+    return (Math.round(p * 10) / 10000000000).toFixed(4)
+}
 
 const currentPoints = {
       'Noraaaaaaaa #1 Rise fan' : 510,
@@ -16,30 +18,27 @@ const second = sortedPoints[1]
 const third = sortedPoints[2]
 const rest = sortedPoints.slice(3)
 
-console.log(first)
-console.log(second)
-console.log(third)
-console.log(rest)
+
 
 document.getElementById('holds-all-boxes').insertAdjacentHTML("beforeend",`
     <div class="first">
         <h1 class="name-a">🥇 ${first[0]} </h1> 
-        <h1 class="points-a">${first[1]}</h1>
+        <h1 class="points-a">${first[1]} | ${percentTillGoal(first[1])}%</h1>
     </div>
     <div class="second">
         <h1 class="name-a">🥈 ${second[0]}</h1>  
-        <h1 class="points-a">${second[1]}</h1>
+        <h1 class="points-a">${second[1]} | ${percentTillGoal(second[1])}%</h1>
     </div>
     <div class="third">
         <h1 class="name-a">🥉 ${third[0]}</h1>  
-        <h1 class="points-a">${third[1]}</h1>
+        <h1 class="points-a">${third[1]} | ${percentTillGoal(third[1])}%</h1>
     </div>`)
 
 rest.forEach(function(placeElement) {
     document.getElementById('holds-all-boxes').insertAdjacentHTML("beforeend",`
     <div class="box-a">
         <h1 class="name-a">${placeElement[0]}</h1>  
-        <h1 class="points-a">${placeElement[1]}</h1>
+        <h1 class="points-a">${placeElement[1]} | ${percentTillGoal(placeElement[1])}%</h1>
     </div>`)
 })
 
